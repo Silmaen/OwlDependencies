232da6d5e7a9ecfc4c05080cec0a6f4b34073d62
Silmaen <genteur.slayer@laposte.net>
2023-02-04 21:15:32 +0100

    Adding glad 0.1.36

---
+++ Libs/glad/conanfile.py
@@ -0,0 +1,60 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.errors import ConanInvalidConfiguration
+
+
+class fmtRecipe(ConanFile):
+    name = "glad"
+    version = "0.1.36_owl"
+
+    # Binary configuration
+    settings = {"os", "compiler", "build_type", "arch"}
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    #source_folder = "../../glad-0.1.36"
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", \
+        "glad-config.cmake.in", \
+        "glad-0.1.36/src/*", \
+        "glad-0.1.36/include/*"
+
+    def validate(self):
+        if self.settings.os not in ["Windows", "Linux"]:
+            raise ConanInvalidConfiguration(F" OS {self.settings.os} not supported")
+        if self.settings.compiler not in ["gcc", "clang"]:
+            raise ConanInvalidConfiguration(F"Compiler {self.settings.compiler} not supported")
+        if self.settings.build_type not in ["Debug", "Release"]:
+            raise ConanInvalidConfiguration(F"Build Type {self.settings.build_type} not supported")
+        if self.settings.arch not in ["x86_64"]:
+            raise ConanInvalidConfiguration(F"Arch {self.settings.arch} not supported")
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+            self.options.shared = True
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        if self.settings.build_type == "Debug":
+            self.cpp_info.libs = ["gladd"]
+        else:
+            self.cpp_info.libs = ["glad"]
