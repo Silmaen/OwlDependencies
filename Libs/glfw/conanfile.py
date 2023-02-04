ac53cac308cc403da15f8e2fde412767073b92c7
Silmaen <genteur.slayer@laposte.net>
2023-02-04 23:28:54 +0100

    Adding glfw 3.3.8

---
+++ Libs/glfw/conanfile.py
@@ -0,0 +1,54 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class SpdlogRecipe(ConanFile):
+    name = "glfw"
+    version = "3.3.8_owl"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    source_folder = "../../glfw-3.3.8"
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "glfw-3.3.8/CMakeLists.txt", \
+        "glfw-3.3.8/cmake_uninstall.cmake.in", \
+        "glfw-3.3.8/src/*", \
+        "glfw-3.3.8/include/*", \
+        "glfw-3.3.8/CMake/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+            self.options.shared = True
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        if self.options.shared:
+            tc.cache_variables["BUILD_SHARED_LIBS"] = "ON"
+        tc.cache_variables["GLFW_BUILD_EXAMPLES"] = "OFF"
+        tc.cache_variables["GLFW_BUILD_TESTS"] = "OFF"
+        tc.cache_variables["GLFW_BUILD_DOCS"] = "OFF"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["glfw"]
+
+    
