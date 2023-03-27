76398cf5c92ec3d408bc6f456d702e661ad6cd03
Silmaen <genteur.slayer@laposte.net>
2023-03-27 22:01:38 +0200

    adding googletest

---
+++ Libs/googletest/googletest.py
@@ -0,0 +1,33 @@
+"""
+Depmanager recipes
+"""
+
+from depmanager.api.recipe import Recipe
+
+
+class GoogleTestShared(Recipe):
+    """
+    Shared version
+    """
+    name = "googletest"
+    version = "1.13.0"
+    source_dir = "googletest"
+    kind = "shared"
+
+    def configure(self):
+        self.cache_variables["BUILD_GMOCK"] = "OFF"
+        self.cache_variables["INSTALL_GTEST"] = "ON"
+
+
+class GoogleTestStatic(Recipe):
+    """
+    Shared version
+    """
+    name = "googletest"
+    version = "1.13.0"
+    source_dir = "googletest"
+    kind = "static"
+
+    def configure(self):
+        self.cache_variables["BUILD_GMOCK"] = "OFF"
+        self.cache_variables["INSTALL_GTEST"] = "ON"
