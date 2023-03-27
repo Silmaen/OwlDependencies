51f3bb9735b6e1596794391f57417d219a345df0
Silmaen <genteur.slayer@laposte.net>
2023-03-27 23:17:38 +0200

    adding imguizmo

---
+++ Libs/imguizmo/imguizmo.py
@@ -0,0 +1,27 @@
+"""
+Depmanager recipes
+"""
+
+from depmanager.api.recipe import Recipe
+
+
+class ImguizmoShared(Recipe):
+    """
+    Shared version
+    """
+    name = "imguizmo"
+    version = "1.84"
+    source_dir = "sources"
+    kind = "shared"
+    dependencies = [{"name": "imgui", "kind": "shared"}]
+
+
+class ImguizmoStatic(Recipe):
+    """
+    Shared version
+    """
+    name = "imguizmo"
+    version = "1.84"
+    source_dir = "sources"
+    kind = "static"
+    dependencies = [{"name": "imgui", "kind": "static"}]
