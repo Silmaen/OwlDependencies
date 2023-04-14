cc252629e5ecfb9f9555f8ded60601a815be67ce
Silmaen <genteur.slayer@laposte.net>
2023-04-14 19:50:38 +0200

    Ready for use

--- Libs/nfd/nfd.py
+++ Libs/nfd/nfd.py
@@ -1,8 +1,12 @@
 """
 Depmanager recipes
 """
-
+from shutil import copyfile
+from tempfile import gettempdir
 from depmanager.api.recipe import Recipe
+from pathlib import Path
+
+here = Path(__file__).parent
 
 
 class NfdShared(Recipe):
@@ -14,9 +18,27 @@ class NfdShared(Recipe):
     source_dir = "nativefiledialog-extended"
     kind = "shared"
 
+    def source(self):
+        """
+        Done at the beginning of the procedure
+        """
+        temp = Path(gettempdir()) / "depbuilder"
+        temp.mkdir(exist_ok=True, parents=True)
+        source = here / Path(self.source_dir)
+        # backup
+        copyfile(source / "src" / "CMakeLists.txt", temp / "nfd_CMakeLists.txt")
+        # use ours
+        copyfile(here / "configs" / "CMakeLists.txt", source / "src" / "CMakeLists.txt")
+
     def configure(self):
         self.cache_variables["NFD_BUILD_TESTS"] = "OFF"
         self.cache_variables["NFD_INSTALL"] = "ON"
+
+    def clean(self):
+        temp = Path(gettempdir()) / "depbuilder"
+        temp.mkdir(exist_ok=True, parents=True)
+        source = here / Path(self.source_dir)
+        copyfile(temp / "nfd_CMakeLists.txt", source / "src" / "CMakeLists.txt")
 
 
 class NfdStatic(Recipe):
@@ -28,7 +50,25 @@ class NfdStatic(Recipe):
     source_dir = "nativefiledialog-extended"
     kind = "static"
 
+    def source(self):
+        """
+        Done at the beginning of the procedure
+        """
+        temp = Path(gettempdir()) / "depbuilder"
+        temp.mkdir(exist_ok=True, parents=True)
+        source = here / Path(self.source_dir)
+        # backup
+        copyfile(source / "src" / "CMakeLists.txt", temp / "nfd_CMakeLists.txt")
+        # use ours
+        copyfile(here / "configs" / "CMakeLists.txt", source / "src" / "CMakeLists.txt")
+
     def configure(self):
         self.cache_variables["NFD_BUILD_TESTS"] = "OFF"
         self.cache_variables["NFD_INSTALL"] = "ON"
+
+    def clean(self):
+        temp = Path(gettempdir()) / "depbuilder"
+        temp.mkdir(exist_ok=True, parents=True)
+        source = here / Path(self.source_dir)
+        copyfile(temp / "nfd_CMakeLists.txt", source / "src" / "CMakeLists.txt")
 
--- Libs/spirv-cross/spirv-cross.py
+++ Libs/spirv-cross/spirv-cross.py
@@ -12,21 +12,8 @@ class SPIRVCrossShared(Recipe):
     name = "spirv-cross"
     version = "1.3.239.0"
     source_dir = "SPIRV-Cross"
-    kind = "shared"
+    kind = "any"
 
     def configure(self):
         self.cache_variables["SPIRV_CROSS_SHARED"] = "ON"
-        self.cache_variables["SPIRV_CROSS_ENABLE_TESTS"] = "OFF"
-
-
-class SPIRVCrossStatic(Recipe):
-    """
-    Shared version
-    """
-    name = "spirv-cross"
-    version = "1.3.239.0"
-    source_dir = "SPIRV-Cross"
-    kind = "static"
-
-    def configure(self):
         self.cache_variables["SPIRV_CROSS_ENABLE_TESTS"] = "OFF"
