6988304241d1e59d7d5fee3990b2d14928acfab0
Silmaen <genteur.slayer@laposte.net>
2023-06-26 23:46:35 +0200

    Correction for CI (#3)

--- Libs/glm/glm.py
+++ Libs/glm/glm.py
@@ -1,8 +1,10 @@
 """
 Depmanager recipes
 """
-
 from depmanager.api.recipe import Recipe
+from pathlib import Path
+
+here = Path(__file__).parent
 
 
 class GlmShared(Recipe):
@@ -11,11 +13,12 @@ class GlmShared(Recipe):
     """
     name = "glm"
     version = "0.9.9.9"
-    source_dir = "sources"
+    source_dir = "sources/glm"
     kind = "shared"
 
     def configure(self):
         self.cache_variables["BUILD_STATIC_LIBS"] = "OFF"
+        self.cache_variables["BUILD_TESTING"] = "OFF"
 
 
 class GlmStatic(Recipe):
@@ -24,9 +27,10 @@ class GlmStatic(Recipe):
     """
     name = "glm"
     version = "0.9.9.9"
-    source_dir = "sources"
+    source_dir = "sources/glm"
     kind = "static"
 
     def configure(self):
         self.cache_variables["BUILD_STATIC_LIBS"] = "ON"
+        self.cache_variables["BUILD_TESTING"] = "OFF"
 
