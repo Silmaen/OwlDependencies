a4cda16285c96d44e8e99890fa64f99229446ae5
Silmaen <genteur.slayer@laposte.net>
2024-01-23 22:29:04 +0100

    Add tiny obj loader (#11)

---
+++ Libs/tinyobjloader/tinyobjloader.py
@@ -0,0 +1,31 @@
+"""
+Depmanager recipes
+"""
+
+from pathlib import Path
+
+from depmanager.api.recipe import Recipe
+
+here = Path(__file__).parent
+
+
+class TinyObjLoaderShared(Recipe):
+    """
+    Shared version
+    """
+
+    name = "tinyobjloader"
+    version = "2.0.0-rc10"
+    source_dir = "tinyobjloader"
+    kind = "shared"
+
+    def configure(self):
+        self.cache_variables["CMAKE_DEBUG_POSTFIX"] = "d"
+
+
+class TinyObjLoaderStatic(TinyObjLoaderShared):
+    """
+    Static  version
+    """
+
+    kind = "static"
