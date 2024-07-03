117501e8626ac5160a269493cd0e017ca591b2ce
Silmaen <genteur.slayer@laposte.net>
2024-07-03 19:54:17 +0200

    better default remote

--- CI/AddDefault.sh
+++ CI/AddDefault.sh
@@ -6,5 +6,5 @@ remote_passwd=$3
 
 set -e
 
-depmanager remote add -vv -n pack -d -u ${remote_url} -l &{remote_login} -p ${remote_passwd}
+depmanager remote add -vv -n pack -d -u ${remote_url} -l ${remote_login} -p ${remote_passwd}
 depmanager remote list -vv
