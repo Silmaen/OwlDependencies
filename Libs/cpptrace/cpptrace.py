e736f43b7a8ee977ce8835768b42d47377b1407c
Silmaen <genteur.slayer@laposte.net>
2024-10-10 00:28:16 +0200

    make cpptrace compile on windows

--- Libs/cpptrace/cpptrace.py
+++ Libs/cpptrace/cpptrace.py
@@ -4,6 +4,17 @@ Depmanager recipes
 
 from depmanager.api.recipe import Recipe
 
+cmakelists_modif = [
+    "src/binary/object.cpp",
+]
+corrections = [
+    [
+        b"#define WIN32_LEAN_AND_MEAN",
+        b"#define WIN32_LEAN_AND_MEAN\n#include <system_error>",
+        None,
+    ],
+]
+
 
 class CppTraceShared(Recipe):
     """
@@ -19,6 +30,41 @@ class CppTraceShared(Recipe):
         {"name": "libdwarf", "kind": "static"},
         {"name": "zlib", "kind": "static"},
     ]
+
+    def source(self):
+        for cmakelists in cmakelists_modif:
+            path = self.path / self.source_dir / cmakelists
+            if not path.exists():
+                print(f"Error: file {cmakelists} @ {path} not found...")
+                continue
+            with open(path, "rb") as fp:
+                lines = fp.read()
+            for correction in corrections:
+                if correction[2] not in [None, ""]:
+                    if correction[2] != cmakelists:
+                        continue
+                lines = lines.replace(correction[0], correction[1])
+            with open(path, "wb") as fp:
+                fp.write(lines)
+            print(f"***** File {cmakelists} @ {path} found and modified.")
+
+    def clean(self):
+        for cmakelists in cmakelists_modif:
+            path = self.path / self.source_dir / cmakelists
+            if not path.exists():
+                print(f"Error: file {cmakelists} @ {path} not found...")
+                continue
+            with open(path, "rb") as fp:
+                lines = fp.read()
+            for correction in corrections:
+                if correction[2] not in [None, ""]:
+                    if correction[2] != cmakelists:
+                        continue
+                lines = lines.replace(correction[1], correction[0])
+                pass
+            with open(path, "wb") as fp:
+                fp.write(lines)
+            print(f"***** File {cmakelists} @ {path} restored.")
 
     def configure(self):
         self.cache_variables["CMAKE_DEBUG_POSTFIX"] = "d"
