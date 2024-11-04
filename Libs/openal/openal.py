4e39f288d139c5f1ce04ffc646c14be4c5e0f029
Silmaen <genteur.slayer@laposte.net>
2024-11-05 00:04:29 +0100

    adding OpenAL

---
+++ Libs/openal/openal.py
@@ -0,0 +1,33 @@
+"""
+Depmanager recipes
+"""
+
+from pathlib import Path
+
+from depmanager.api.recipe import Recipe
+
+here = Path(__file__).parent
+
+
+class OpenALShared(Recipe):
+    """
+    Shared version
+    """
+
+    name = "openal"
+    version = "1.23.1"
+    source_dir = "openal-soft"
+    kind = "shared"
+
+    def configure(self):
+        self.cache_variables["ALSOFT_EXAMPLES"] = "OFF"
+        self.cache_variables["ALSOFT_INSTALL_EXAMPLES"] = "OFF"
+        self.cache_variables["CMAKE_DEBUG_POSTFIX"] = "d"
+
+
+class OpenALStatic(OpenALShared):
+    """
+    Static  version
+    """
+
+    kind = "static"
