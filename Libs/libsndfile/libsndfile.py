447985ad80fa6cfc714008fc1b93167b90f382a8
Silmaen <genteur.slayer@laposte.net>
2025-01-10 15:57:56 +0100

    add libsndfile

---
+++ Libs/libsndfile/libsndfile.py
@@ -0,0 +1,33 @@
+"""
+Depmanager recipes
+"""
+
+from depmanager.api.recipe import Recipe
+
+
+class LibSndFileShared(Recipe):
+    """
+    Shared version
+    """
+
+    name = "libsndfile"
+    version = "1.2.2"
+    source_dir = "libsndfile"
+    kind = "shared"
+
+    def configure(self):
+        self.cache_variables["BUILD_TESTING"] = "OFF"
+        self.cache_variables["INSTALL_PKGCONFIG_MODULE"] = "OFF"
+        self.cache_variables["BUILD_EXAMPLES"] = "OFF"
+        self.cache_variables["BUILD_PROGRAMS"] = "OFF"
+        self.cache_variables["BUILD_REGTEST"] = "OFF"
+        self.cache_variables["INSTALL_MANPAGES"] = "OFF"
+        self.cache_variables["CMAKE_DEBUG_POSTFIX"] = "d"
+
+
+class LibSndFileStatic(LibSndFileShared):
+    """
+    Static version
+    """
+
+    kind = "static"
