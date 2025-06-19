ac2eec983faf945eca1319e2f8056e96ded1c0e2
Silmaen <genteur.slayer@laposte.net>
2025-06-19 22:12:33 +0200

    Upgrade minimal cmake version to 3.5 on the last old versions...

--- Libs/libsndfile/libsndfile.py
+++ Libs/libsndfile/libsndfile.py
@@ -4,6 +4,17 @@ Depmanager recipes
 
 from depmanager.api.recipe import Recipe
 
+file_modif = [
+    "CMakeLists.txt",
+]
+corrections = [
+    [
+        b"cmake_minimum_required (VERSION 3.1..3.18)",
+        b"cmake_minimum_required (VERSION 3.5)",
+        None,
+    ],
+]
+
 
 class LibSndFileShared(Recipe):
     """
@@ -14,6 +25,43 @@ class LibSndFileShared(Recipe):
     version = "1.2.2"
     source_dir = "libsndfile"
     kind = "shared"
+
+    def source(self):
+        # Files to modify
+        for file in file_modif:
+            path = self.path / self.source_dir / file
+            if not path.exists():
+                print(f"Error: file {file} @ {path} not found...")
+                continue
+            with open(path, "rb") as fp:
+                lines = fp.read()
+            for correction in corrections:
+                if correction[2] not in [None, ""]:
+                    if correction[2] != file:
+                        continue
+                lines = lines.replace(correction[0], correction[1])
+            with open(path, "wb") as fp:
+                fp.write(lines)
+            print(f"***** File {file} @ {path} found and modified.")
+
+    def clean(self):
+        # Files to restore
+        for file in file_modif:
+            path = self.path / self.source_dir / file
+            if not path.exists():
+                print(f"Error: file {file} @ {path} not found...")
+                continue
+            with open(path, "rb") as fp:
+                lines = fp.read()
+            for correction in corrections:
+                if correction[2] not in [None, ""]:
+                    if correction[2] != file:
+                        continue
+                lines = lines.replace(correction[1], correction[0])
+                pass
+            with open(path, "wb") as fp:
+                fp.write(lines)
+            print(f"***** File {file} @ {path} restored.")
 
     def configure(self):
         self.cache_variables["BUILD_TESTING"] = "OFF"
